63f314e0dde3dd7051ba9158e1c641fd19bc4286
gugucrypto <Gugulethu Mathe>
2022-12-22 15:51:41 +0200

    Send Emails

--- app/Controllers/Book.php
+++ app/Controllers/Book.php
@@ -33,7 +33,7 @@ class Book extends BaseController
     }
     public function saveOrder()
     {
-        
+
         $request = \Config\Services::request();
         $order = [
             'customer_id' => $request->getVar('customer_id'),
@@ -61,7 +61,7 @@ class Book extends BaseController
         /* Make sure you clear the following session after successful save
         $_SESSION['customer_id']
          */
-		$this->updateEventStatus();
+        $this->updateEventStatus();
         return redirect()->to('/thank-you');
     }
 
@@ -84,14 +84,13 @@ class Book extends BaseController
             'end_date' => date("Y-m-d", strtotime($request->getVar('end_date'))),
             'number' => $request->getVar('number'),
         ];
-     
+
         $model->save($newData);
         // return $this->response->setJSON($data);
         // $session->setFlashdata("success", "Successfully");
         $session->set(['customer_id' => $model->getInsertID()]);
 
         return redirect()->to('/choose-package');
-
     }
 
     public function approveOrder()
@@ -101,7 +100,7 @@ class Book extends BaseController
 
         $request = \Config\Services::request();
         $session = session();
-		$id = $request->getVar('customer_id');
+        $id = $request->getVar('customer_id');
         $data = array(
             'event_status' => 'Approved',
             'start_date' => date("Y-m-d", strtotime($request->getVar('start_date'))),
@@ -119,10 +118,10 @@ class Book extends BaseController
 
         $request = \Config\Services::request();
         $session = session();
-		$id = $request->getVar('customer_id');
+        $id = $request->getVar('customer_id');
         $data = array(
             'event_status' => 'Check_out',
-        
+
         );
         $model->update($id, $data);
         $session->setFlashdata("check_out", "Customer check out Successfully");
@@ -130,22 +129,102 @@ class Book extends BaseController
     }
 
 
-	public function updateEventStatus()
+    public function updateEventStatus()
     {
         $data = [];
         $model = new CustomerModel();
 
         $request = \Config\Services::request();
+        $email = \Config\Services::email(); 
         $session = session();
-		$id = $request->getVar('customer_id');
+        $id =  $request->getVar('customer_id');
         $data = array(
-            'event_status' => 'Pending',            
+            'event_status' => 'Pending',
         );
         $model->update($id, $data);
-       
+
+        $db = \Config\Database::connect();
+        $query = $db->query("SELECT * FROM customers WHERE  id='" . $id . "'");
+        $results = $query->getResult();
+        $first_name = "";
+        $last_name = "";
+        $phone = "";
+        $cus_email = "";
+        $created_at = "";
+        $start_date = "";
+        $end_date = "";
+        $number = "";
+        
+        foreach ($results as $cus) {
+            $first_name = $cus->first_name;
+            $last_name = $cus->last_name;
+            $phone = $cus->phone;
+            $cus_email = $cus->email;
+            $created_at = $cus->created_at;
+            $start_date = $cus->start_date;
+            $end_date = $cus->end_date;
+            $number = $cus->number;
+        }
+
+
+        $email = \Config\Services::email(); // loading for use
+        $request = \Config\Services::request();
+
+        // This is for getting form data while submit
+        $full_name = $first_name . " " .  $last_name;
+
+
+        $email->setTo($cus_email);
+        $email->setFrom('info@izikodelounge.co.za', 'Iziko de Lounge');
+
+        $data = array(
+            'full_name' => $first_name,
+
+            'phone' => $phone,
+            'cus_email' => $cus_email,
+            'created_at' => $created_at,
+            'start_date' => $start_date,
+            'end_date' => $end_date,
+            'number' => $number
+
+        );
+        $data1['data'] = $data;
+
+        $email->setSubject("Application submitted successfully");
+        $email->setMessage(view('template/customer', $data1));
+
+        if ($email->send()) {
+            //Send to admins
+            $email->clear(TRUE);
+            // $email->setTo(['butho@mpholdings.co.za', 'gugulethumath@gmail.com','anthony.mpofu@gmail.com','calvinhm64@gmail.com','calvinhm64@gmail.com']);
+            $email->setTo(['info@izikodelounge.co.za']);
+            $email->setFrom('info@izikodelounge.co.za', 'Iziko de Lounge');
+            // If you need to send mail to CC and BCC
+            // $email->setCC('another@another-user.com');
+            // $email->setBCC('other@other-user.com');
+            $data = array(
+                'full_name' => $first_name,
+
+                'phone' => $phone,
+                'cus_email' => $cus_email,
+                'created_at' => $created_at,
+                'start_date' => $start_date,
+                'end_date' => $end_date,
+                'number' => $number
+            );
+            $data_admin['data'] = $data;
+
+            $email->setSubject("New application on Iziko de Lounge");
+            $email->setMessage(view('template/admin', $data_admin));
+            $email->send();
+            $email->clear(TRUE);
+        } else {
+            $data = $email->printDebugger(['headers']);
+            print_r($data);
+        }
         // return redirect()->to('/apartments');
     }
-    
+
     public function select_package()
     {
         // $model = new CustomerModel();
@@ -176,6 +255,5 @@ class Book extends BaseController
         ];
         $model->save($newData);
         return $this->response->setJSON($data);
-
     }
 }
